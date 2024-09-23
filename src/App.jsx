46ef610013392db4a533bebc75cb11491b7592dd
yashdarji30 <123467783+yashdarji30@users.noreply.github.com>
2024-09-23 11:27:13 +0530

    2nd Last Fully created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import react from 'react';
+import Header from './sections/Header';
+import Hero from './sections/Hero';
+import Features from './sections/Features';
+import Pricing from './sections/Pricing';
+import Faq from './sections/Faq';
+import Download from './sections/Download';
+import Footer from './sections/Footer';
 
-function App() {
+const App = () => {
   return(
-    <div>Hello</div>
+    <mai className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Download />
+      <Footer />
+    </mai>
   )
 }
 
